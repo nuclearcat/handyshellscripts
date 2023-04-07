ecc5935ac39b976b7ba1bf0d60f887c0964abb97
Denys Fedoryshchenko <nuclearcat@nuclearcat.com>
2023-04-07 22:48:42 +0000

    Fix by shellcheck

--- backup.sh
+++ backup.sh
@@ -2,10 +2,11 @@
 # Check if we are root
 if [ "$(id -u)" != "0" ] ; then
   echo "You are not root, exiting"
-  #exit 1
+  exit 1
 fi
-# Lookup /proc/self/exe
-let proc_self_exe="$(readlink /proc/self/exe)"
+# Lookup /proc/self/exe assign to variable proc_self_exe
+proc_self_exe=$(readlink /proc/self/exe)
+
 if [ "$proc_self_exe" == "/usr/local/bin/backup.sh" ] ; then
   echo "You are not /usr/local/bin/backup.sh, but $0 exiting"
   exit 1
@@ -13,15 +14,15 @@ fi
 
 # Check if backup.service exist in systemd
 if [ ! -f /etc/systemd/system/backup.service ] ; then
-  echo "Backup.service does not exist, creating"
+  printf "Backup.service does not exist, creating"
   # Create backup.service
-  echo "[Unit]\nDescription=Backup\n" > /etc/systemd/system/backup.service  
-  echo "[Service]\nExecStart=/usr/local/bin/backup.sh\nType=oneshot\n" >> /etc/systemd/system/backup.service
-  echo "[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/backup.service
+  printf "[Unit]\nDescription=Backup\n" > /etc/systemd/system/backup.service
+  printf "[Service]\nExecStart=/usr/local/bin/backup.sh\nType=oneshot\n" >> /etc/systemd/system/backup.service
+  printf "[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/backup.service
   # Create backup.timer that runs each day at 3am
-  echo "[Unit]\nDescription=Backup\n" > /etc/systemd/system/backup.timer
-  echo "[Timer]\nOnCalendar=*-*-* 03:00:00" >> /etc/systemd/system/backup.service
-  echo "[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/backup.timer
+  printf "[Unit]\nDescription=Backup\n" > /etc/systemd/system/backup.timer
+  printf "[Timer]\nOnCalendar=*-*-* 03:00:00" >> /etc/systemd/system/backup.service
+  printf "[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/backup.timer
   # Reload
   systemctl daemon-reload
   # Enable timer
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
     deps = [
+        pkgs.haskellPackages.ShellCheck
         pkgs.sudo
         pkgs.bashInteractive
         pkgs.man
