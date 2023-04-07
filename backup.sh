658a037a2f2510ca6448d92767c11d75544f0923
Denys Fedoryshchenko <nuclearcat@nuclearcat.com>
2023-04-07 22:53:28 +0000

    Improve self-install

--- backup.sh
+++ backup.sh
@@ -7,9 +7,15 @@ fi
 # Lookup /proc/self/exe assign to variable proc_self_exe
 proc_self_exe=$(readlink /proc/self/exe)
 
-if [ "$proc_self_exe" == "/usr/local/bin/backup.sh" ] ; then
-  echo "You are not /usr/local/bin/backup.sh, but $0 exiting"
-  exit 1
+if [ "$proc_self_exe" != "/usr/local/bin/backup.sh" ] ; then
+  echo "You are not /usr/local/bin/backup.sh, but $0"
+  # Does /usr/local/bin/backup.sh exist? if not install from
+  # https://raw.githubusercontent.com/nuclearcat/handyshellscripts/main/backup.sh
+  if [ ! -f /usr/local/bin/backup.sh ] ; then
+  echo "Installing backup.sh" 
+  wget -O /usr/local/bin/backup.sh https://raw.githubusercontent.com/nuclearcat/handyshellscripts/main/backup.sh
+  chmod +x /usr/local/bin/backup.sh
+  fi
 fi
 
 # Check if backup.service exist in systemd
@@ -21,7 +27,7 @@ if [ ! -f /etc/systemd/system/backup.service ] ; then
   printf "[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/backup.service
   # Create backup.timer that runs each day at 3am
   printf "[Unit]\nDescription=Backup\n" > /etc/systemd/system/backup.timer
-  printf "[Timer]\nOnCalendar=*-*-* 03:00:00" >> /etc/systemd/system/backup.service
+  printf "[Timer]\nOnCalendar=*-*-* 03:00:00" >> /etc/systemd/system/backup.timer
   printf "[Install]\nWantedBy=multi-user.target" >> /etc/systemd/system/backup.timer
   # Reload
   systemctl daemon-reload
